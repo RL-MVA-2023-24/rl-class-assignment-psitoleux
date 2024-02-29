be8cdd9a576f18ddfe0cd8d506a84a764c12e282
psitoleux <paul.sitoleux+github@me.com>
2024-02-29 19:12:12 +0100

    added buffer size

--- src/parsers.py
+++ src/parsers.py
@@ -18,5 +18,8 @@ def get_train_parser():
     parser.add_argument('--wd', type=float, default=0.01, help='weight decay')
     parser.add_argument('--nb_epoch', type=int, default=100, help='number of epochs')
     parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    
+    parser.add_argument('--buffer_size', type=int, default=1000000, help='replay buffer size')
+
 
     return parser.parse_args()
